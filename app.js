c549faae17de3e200ef3b3e57c26638ff318995f
Anuradha Weeraman <aweeraman@gmail.com>
2015-07-15 23:13:28 +0530

    Integrated passport for Facebook authentication

--- app.js
+++ app.js
@@ -1,14 +1,67 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var appcfg        = require('./config/appcfg.js');
+var express       = require('express');
+var session       = require('express-session');
+var cookieParser  = require('cookie-parser');
+var bodyParser    = require('body-parser');
+var passport      = require('passport');
+var fbStrategy    = require('passport-facebook').Strategy;
 
 var app = express();
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new fbStrategy({
+  clientID: appcfg.fb_api_key,
+  clientSecret: appcfg.fb_api_secret,
+  callbackURL: appcfg.callback_url
+},
+function(accessToken, refreshToken, profile, done) {
+  process.nextTick(function() {
+    // Check whether the user exists using the profile.id
+    return done(null, profile);
+  });
+}));
 
 var ping = require('./routes/ping');
 
 app.set('port', process.env.PORT || 8080);
 
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session(
+  { secret: appcfg.session_secret,
+    key: 'session_id',
+    resave: true,
+    saveUninitialized: true
+  }
+));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect: '/app',
+    failureRedirect: '/auth/error'
+  }), function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/app', function(req, res) {
+  res.send("Welcome.");
+});
+
+app.get('/auth/error', function(req, res) {
+  res.send("Authentication failed.");
+});
 
 app.use('/', express.static('public'));
 app.use('/api', ping);
---
+++ config/appcfg.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "session_secret": "s3ssI0n_Sikri7",
+  "fb_api_key" : "403530669838044",
+  "fb_api_secret": "29f9c824f7f681da40237ef283a6832d",
+  "callback_url": "http://localhost:8080/auth/facebook/callback",
+};
